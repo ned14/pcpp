d1e22b3fa141c68975e173186a33f2ba3c69ac3d
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2019-04-18 10:36:51 +0100

    Add test demoing solution to issue #27

---
+++ tests/issue0027.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import, print_function
+import unittest, time
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+try:
+    clock = time.process_time
+except:
+    clock = time.clock
+
+class runner(object):
+    def runTest(self):
+        from pcpp import Preprocessor, OutputDirective, Action
+        import os, sys
+
+        class PassThruPreprocessor(Preprocessor):
+            def on_directive_handle(self,directive,toks,ifpassthru,precedingtoks):
+                if len(precedingtoks) == 1:
+                    # Execute
+                    return super(PassThruPreprocessor, self).on_directive_handle(directive,toks,ifpassthru,precedingtoks)
+                raise OutputDirective(Action.IgnoreAndPassThrough)
+
+        start = clock()
+        p = PassThruPreprocessor()
+        p.parse(self.input)
+        oh = StringIO()
+        p.write(oh)
+        end = clock()
+        print("Preprocessed test in", end-start, "seconds")
+        if oh.getvalue() != self.output:
+            print("Should be:\n" + self.output + "EOF\n", file = sys.stderr)
+            print("\nWas:\n" + oh.getvalue()+"EOF\n", file = sys.stderr)
+        self.assertEqual(p.return_code, 0)
+        self.assertEqual(oh.getvalue(), self.output)
+
+            
+class space_after_hash(unittest.TestCase, runner):
+    input = r"""#if 5
+1
+#endif
+# if 5
+2
+# endif
+#warning Hi
+# warning Hi2"""
+    output = r"""
+1
+
+# if 5
+2
+# endif
+
+# warning Hi2
+"""
