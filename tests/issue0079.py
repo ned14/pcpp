9c702df558cd5957502d3bf7d3f59a70eac92440
Will Wray <wjwray@gmail.com>
2022-12-15 08:02:14 -0400

    Revert "Update test to use context manager for StringIO"
    This reverts commit 8a5b9d0643066a06de40490110a05ffbf396fd61.

--- tests/issue0079.py
+++ tests/issue0079.py
@@ -12,17 +12,17 @@ class runner(object):
 
         p = Preprocessor()
         p.parse(self.input)
-        with StringIO() as oh:
-            p.write(oh)
-            print("ok")
-            if oh.getvalue() != self.output:
-                print("Should be:\n" + self.output + "EOF\n", file = sys.stderr)
-                print("\nWas:\n" + oh.getvalue()+"EOF\n", file = sys.stderr)
-            self.assertEqual(p.return_code, 0)
-            self.assertEqual(oh.getvalue(), self.output)
+        oh = StringIO()
+        p.write(oh)
+        print("ok")
+        if oh.getvalue() != self.output:
+            print("Should be:\n" + self.output + "EOF\n", file = sys.stderr)
+            print("\nWas:\n" + oh.getvalue()+"EOF\n", file = sys.stderr)
+        self.assertEqual(p.return_code, 0)
+        self.assertEqual(oh.getvalue(), self.output)
 
 class pp_number_pasting(unittest.TestCase, runner):
-    input = """#define CAT_(A,B)A##B
+    input = r"""#define CAT_(A,B)A##B
 #define CAT(A,B)CAT_(A,B)
 #define Ox 0x
 #if CAT(Ox,1)
@@ -31,7 +31,7 @@ PP_NUMBER pasted ok
 PP_NUMBER paste fail
 #endif
 """
-    output = """
+    output = r"""
 
 
 
